c2c82fd7a85880eebb43d74007f189b4e640816b
Devpod17 <shubhama664@gmail.com>
2024-11-10 11:37:27 +0530

    adds previous drawings

--- controllers/roomController.js
+++ controllers/roomController.js
@@ -1,4 +1,4 @@
-const { userJoin, userLeave, getUsers } = require("../utils/user");
+const { userJoin, userLeave, getUsers, addDrawing, getDrawings } = require("../utils/user");
 
 function handleUserJoined(socket, io, data) {
     const { roomId, userId, userName } = data;
@@ -6,8 +6,18 @@ function handleUserJoined(socket, io, data) {
     socket.join(user.room);
 
     const roomUsers = getUsers(user.room);
+    
+    // Emit a welcome message to the newly joined user
     socket.emit("message", { message: "Welcome to ChatRoom" });
+    
+    // Emit previous drawings only to the newly joined user
+    const previousDrawings = getDrawings(user.room);
+    socket.emit("previous-drawings", previousDrawings);
+    
+    // Notify other users in the room of the new user's arrival
     socket.broadcast.to(user.room).emit("message", { message: `${user.username} has joined` });
+    
+    // Emit the updated list of users in the room
     io.to(user.room).emit("users", roomUsers);
 }
 
@@ -15,8 +25,14 @@ function handleUserLeave(socket, io) {
     const user = userLeave(socket.id);
     if (user) {
         const roomUsers = getUsers(user.room);
+        
+        // Notify other users in the room that this user has left
         io.to(user.room).emit("message", { message: `${user.username} left the chat` });
+        
+        // Emit the updated list of users in the room
         io.to(user.room).emit("users", roomUsers);
+        
+        // Leave the socket room
         socket.leave(user.room);
     }
 }
@@ -24,6 +40,10 @@ function handleUserLeave(socket, io) {
 function handleDraw(socket, data) {
     const userRoom = Array.from(socket.rooms)[1];
     if (userRoom) {
+        // Store the drawing data for this room
+        addDrawing(userRoom, data);
+
+        // Broadcast the new drawing to other users in the room
         socket.broadcast.to(userRoom).emit("draw", data);
     }
 }
--- utils/user.js
+++ utils/user.js
@@ -1,39 +1,55 @@
 const users = [];
+const drawings = {};
 
 // Join user to chat
 const userJoin = (id, username, room, host, presenter) => {
   const user = { id, username, room, host, presenter };
-
   users.push(user);
   return user;
 };
+
 // User leaves chat
 const userLeave = (id) => {
   const index = users.findIndex((user) => user.id === id);
-
   if (index !== -1) {
     return users.splice(index, 1)[0];
   }
 };
+
+// Get user by ID
 const getUserById = (userId) => {
-  return users.find(user => user.userId === userId);
-}
+  return users.find(user => user.id === userId);
+};
 
-//get users
+// Get all users in a room
 const getUsers = (room) => {
-  const RoomUsers = [];
-  users.map((user) => {
-    if (user.room == room) {
-      RoomUsers.push(user);
-    }
-  });
+  return users.filter((user) => user.room === room);
+};
 
-  return RoomUsers;
+// Add a drawing to a specific room
+const addDrawing = (room, drawingData) => {
+  if (!drawings[room]) {
+    drawings[room] = [];
+  }
+  drawings[room].push(drawingData);
+};
+
+// Get all drawings for a specific room
+const getDrawings = (room) => {
+  return drawings[room] || [];
+};
+
+// Clear drawings for a room (optional: in case of reset or new session)
+const clearDrawings = (room) => {
+  drawings[room] = [];
 };
 
 module.exports = {
   userJoin,
   userLeave,
+  getUserById,
   getUsers,
-  getUserById
+  addDrawing,
+  getDrawings,
+  clearDrawings
 };
